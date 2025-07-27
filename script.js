62fa24a78d1aa98efa93cffcf4fb5e6a70a124b4
sukiyaki-Lunatic <sukiyakinormal2gou@gmail.com>
2025-07-27 16:37:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,20 @@ sliders.forEach(id => {
 });
 
 function downloadPDF() {
-  const element = document.getElementById("seihekiForm");
+  const button = document.querySelector("button");
+  button.style.display = "none"; // ボタンを非表示にする
+
+  const element = document.body;
+
   const opt = {
     margin:       0.5,
-    filename:     '性癖シート.pdf',
+    filename:     'seiheki-sheet.pdf',
     image:        { type: 'jpeg', quality: 0.98 },
     html2canvas:  { scale: 2 },
     jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
   };
-  html2pdf().set(opt).from(element).save();
+
+  html2pdf().set(opt).from(element).save().then(() => {
+    button.style.display = "inline-block"; // PDF保存後に再表示
+  });
 }
